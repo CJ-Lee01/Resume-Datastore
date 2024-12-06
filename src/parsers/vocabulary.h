07fbb41f4dafd40248aaa77b022f24d40a0c7f93
CJ-Lee01 <chunjie01@live.com>
2024-12-06 17:24:11 -0500

    Add vocabulary

---
+++ src/parsers/vocabulary.h
@@ -0,0 +1,31 @@
+#ifndef VOCAB_H
+#define VOCAB_H
+
+// define your own namespace to hold constants
+namespace vocab
+{
+  #include <string>
+  using std::string;
+    // constants have internal linkage by default
+    const string ADD_CMD_KEYWORD { "add" };
+    const string RM_CMD_KEYWORD { "rm" };
+    const string UPDATE_CMD_KEYWORD { "update" };
+    const string EXIT_CMD_KEYWORD { "exit" };
+    const string CLEAN_CMD_KEYWORD { "clean" };
+    const string LIST_CMD_KEYWORD { "clean" };
+    const string SEARCH_CMD_KEYWORD { "search" };
+    const string HELP_CMD_KEYWORD { "help" };
+
+    const string TYPE_FULL_TAG { "type" };
+    const string KEYWORD_FULL_TAG { "keyword" };
+    const string IMPORTANT_FULL_TAG { "important" };
+    const string PARENT_FULL_TAG { "parent" };
+
+    const string TYPE_SHORT_TAG { "t" };
+    const string KEYWORD_SHORT_TAG { "kw" };
+    const string IMPORTANT_SHORT_TAG { "i" };
+    const string PARENT_SHORT_TAG { "p" };
+
+    // ... other related constants
+}
+#endif
